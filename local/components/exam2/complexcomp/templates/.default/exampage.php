d21dbc0a9f57073e0226583d1be10ee0fc971319
Your Name <you@example.com>
2024-05-23 13:45:44 +0300

    ex2-25

---
+++ local/components/exam2/complexcomp/templates/.default/exampage.php
@@ -0,0 +1,7 @@
+<?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();?>
+<?
+echo 'SECTION_ID = ' . $arResult['VARIABLES']['SECTION_ID'] . '<br/>';
+echo 'ELEMENT_ID = ' . $arResult['VARIABLES']['ELEMENT_ID'] . '<br/>';
+echo 'PARAM1 = ' . $arResult['VARIABLES']['PARAM1'] . '<br/>';
+echo 'PARAM2 = ' . $arResult['VARIABLES']['PARAM2'];
+?>
