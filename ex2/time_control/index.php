56b135aac9bf29f461582ef14abb842cb3d923da
Your Name <you@example.com>
2024-05-23 13:17:24 +0300

    ex2-11

--- ex2/time_control/index.php
+++ ex2/time_control/index.php
@@ -7,5 +7,6 @@ $APPLICATION->SetTitle("Оценка производительности");
 Кеш: 120 КБ Кэш 72 КБ
 Самая долгая странциа /products/index.php 0.0598 сек
     bitrix:catalog: 0.011
-
+    /products/index.php	1	39.56%	23	0.0598
+    bitrix:catalog.section: 0.0061 с; Запросов: 23
 <?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
