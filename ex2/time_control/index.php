ecf2a54012c5d1bdce51fe13ebd55ebed45e862e
Your Name <you@example.com>
2024-05-23 13:11:07 +0300

    ex2-88

---
+++ ex2/time_control/index.php
@@ -0,0 +1,9 @@
+<?
+require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
+$APPLICATION->SetTitle("Оценка производительности");
+?>
+
+Самая ресурсоемкая страница /products/index.php	Нагрузка: 39.56%
+Кеш: 120 КБ Кэш 72 КБ
+
+<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
