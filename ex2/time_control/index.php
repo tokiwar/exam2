506b6745a7c8da5ee794c1f35654dffad2b79701
Your Name <you@example.com>
2024-05-23 13:15:38 +0300

    ex2-10

--- ex2/time_control/index.php
+++ ex2/time_control/index.php
@@ -5,5 +5,7 @@ $APPLICATION->SetTitle("Оценка производительности");
 
 Самая ресурсоемкая страница /products/index.php	Нагрузка: 39.56%
 Кеш: 120 КБ Кэш 72 КБ
+Самая долгая странциа /products/index.php 0.0598 сек
+    bitrix:catalog: 0.011
 
 <?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
